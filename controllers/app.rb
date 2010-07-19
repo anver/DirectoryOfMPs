8888e25391fb7fdc4f86dddf9230347c076a970b
Akshay Rawat <akshay@activesphere.com>
2010-07-20 00:41:33 +0530

    show mp

--- controllers/app.rb
+++ controllers/app.rb
@@ -16,7 +16,14 @@ get '/states/:state_id/mps/' do |state_id|
   haml :mps
 end
 
+get '/mps/:id' do |id|
+  @mp = get "/mps/#{id}"
+  haml :mp
+end
+
 def get url
   response = HTTParty.get "http://api.myminister.info#{url}.json"
+  p "NEW REQUEST<<<<<<<<<<<<http://api.myminister.info#{url}.json>>>"
+  p response.body.inspect
   Crack::JSON.parse(response.body)
 end
